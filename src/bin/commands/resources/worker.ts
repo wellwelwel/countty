055640434671be2d35444160429a2e8591809d84
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2025-09-30 07:28:19 -0300

    refactor: add rate limiting to worker routes

--- src/bin/commands/resources/worker.ts
+++ src/bin/commands/resources/worker.ts
@@ -11,17 +11,14 @@ type CounttyRoutes = Record<
   }) => Promise<Response>
 >;
 
-const { Countty, routes } = createCountty();
+const { Countty, routes, rateLimiter } = createCountty();
 
 const Worker: ExportedHandler<Env> = {
   async fetch(request: Request, env: Env): Promise<Response> {
-    const url = new URL(request.url);
     const context = { request, env, Countty };
+    const rateLimit = rateLimiter(request);
+    const url = new URL(request.url);
     const { pathname } = url;
-
-    const notFound = new Response(JSON.stringify({ message: 'Not found.' }), {
-      status: 404,
-    });
 
     const routeHandlers: CounttyRoutes = {
       '/create': routes.create,
@@ -31,7 +28,22 @@ const Worker: ExportedHandler<Env> = {
       '/reset': routes.reset,
     };
 
-    return routeHandlers[pathname](context) || notFound;
+    // Countty Routes
+    if (pathname in routeHandlers) {
+      if (!rateLimit.available)
+        return new Response(
+          JSON.stringify({
+            message: 'Request limit exceeded. Please try again later.',
+          }),
+          { status: 429 }
+        );
+
+      return routeHandlers[pathname](context);
+    }
+
+    return new Response(JSON.stringify({ message: 'Not found.' }), {
+      status: 404,
+    });
   },
 };
 
